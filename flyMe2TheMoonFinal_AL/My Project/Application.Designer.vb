9266469711480e48c8c7049eedf50535caaec32e
AntonProLysenko <probold777@gmail.com>
2024-12-05 12:09:58 -0500

    Datapickers working as expected

--- flyMe2TheMoonFinal_AL/My Project/Application.Designer.vb
+++ flyMe2TheMoonFinal_AL/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.flyMe2TheMoon_AL.frmMain
+            Me.MainForm = Global.flyMe2TheMoon_AL.frmAdminAddFutureFlight
         End Sub
     End Class
 End Namespace
--- flyMe2TheMoonFinal_AL/frmAdminAddFutureFlight.vb
+++ flyMe2TheMoonFinal_AL/frmAdminAddFutureFlight.vb
@@ -12,45 +12,44 @@
         Dim dtPlanes As DataTable = New DataTable
 
 
-        Try
-            CheckOpenDBConnection(Me)
-            dtAirports = ExecuteSelectProdcedure("uspFindAllAirports")
-            dtFromAirports = dtAirports.Copy()
-            'Loadingg States results to a combobox
-            cmbFromAirport.ValueMember = "intAirportID"
-            cmbFromAirport.DisplayMember = "airportName"
-            cmbFromAirport.DataSource = dtFromAirports
+        'Try
+        '    CheckOpenDBConnection(Me)
+        '    dtAirports = ExecuteSelectProdcedure("uspFindAllAirports")
+        '    dtFromAirports = dtAirports.Copy()
+        '    'Loadingg States results to a combobox
+        '    cmbFromAirport.ValueMember = "intAirportID"
+        '    cmbFromAirport.DisplayMember = "airportName"
+        '    cmbFromAirport.DataSource = dtFromAirports
 
-            'If cmbFromAirport.Items.Count > 0 Then
-            '    cmbFromAirport.SelectedIndex = 0
-            'End If
+        '    'If cmbFromAirport.Items.Count > 0 Then
+        '    '    cmbFromAirport.SelectedIndex = 0
+        '    'End If
 
-            cmbToAirport.ValueMember = "intAirportID"
-            cmbToAirport.DisplayMember = "airportName"
-            cmbToAirport.DataSource = dtAirports
+        '    cmbToAirport.ValueMember = "intAirportID"
+        '    cmbToAirport.DisplayMember = "airportName"
+        '    cmbToAirport.DataSource = dtAirports
 
-            'If cmbFromAirport.Items.Count > 0 AndAlso cmbToAirport.Items.Count > 0 Then
-            '    cmbToAirport.SelectedIndex = 1
-            'End If
+        '    'If cmbFromAirport.Items.Count > 0 AndAlso cmbToAirport.Items.Count > 0 Then
+        '    '    cmbToAirport.SelectedIndex = 1
+        '    'End If
 
 
-            dtPlanes = ExecuteSelectProdcedure("uspFindAllPlanes")
-            cmbPlanes.ValueMember = "intPlaneID"
-            cmbPlanes.DisplayMember = "planeName"
-            cmbPlanes.DataSource = dtPlanes
-            CloseDatabaseConnection()
+        '    dtPlanes = ExecuteSelectProdcedure("uspFindAllPlanes")
+        '    cmbPlanes.ValueMember = "intPlaneID"
+        '    cmbPlanes.DisplayMember = "planeName"
+        '    cmbPlanes.DataSource = dtPlanes
+        '    CloseDatabaseConnection()
 
-        Catch ex As Exception
-            MessageBox.Show(ex.Message)
-        End Try
+        'Catch ex As Exception
+        '    MessageBox.Show(ex.Message)
+        'End Try
     End Sub
 
-    'Private Sub dtpFlightDate_ValueChanged(sender As Object, e As EventArgs) Handles dtpFlightDate.ValueChanged
-    '    dtpDepartureTime.MinDate = dtpFlightDate.Value.Date
-    '    dtpDepartureTime.MaxDate = dtpFlightDate.Value.Date
-    '    dtpLandingTme.MinDate = dtpDepartureTime.Value.Date
-    '    dtpLandingTme.MaxDate = dtpDepartureTime.Value.Date.AddHours(24)
-    'End Sub
+    Private Sub dtpFlightDate_ValueChanged(sender As Object, e As EventArgs) Handles dtpFlightDate.ValueChanged
+        dtpDepartureTime.MaxDate = dtpFlightDate.Value.Date.AddHours(23).AddMinutes(59).AddSeconds(59)
+        dtpDepartureTime.Value = dtpFlightDate.Value
+        dtpDepartureTime.MinDate = dtpFlightDate.Value.Date
+    End Sub
 
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
         Dim intFlightID As Integer = 1
@@ -100,5 +99,36 @@
             CheckOpenDBConnection(Me)
             ExecuteUltimateTransaction(strExecuteInsert, "Flight", "", "Insert")
         End If
+    End Sub
+
+
+    Private Sub dtpDepartureTime_ValueChanged(sender As Object, e As EventArgs) Handles dtpDepartureTime.ValueChanged
+        'MessageBox.Show(dtpDepartureTime.MaxDate)
+        dtpLandingTme.MinDate = dtpDepartureTime.Value.Date
+        dtpLandingTme.MaxDate = dtpDepartureTime.MaxDate.AddDays(1)
+    End Sub
+
+
+
+    Dim blnUpdated As Boolean = False
+    Private Sub dtpLandingTme_ValueChanged(sender As Object, e As EventArgs) Handles dtpLandingTme.ValueChanged
+
+
+        If blnUpdated Then Exit Sub
+
+        If dtpLandingTme.Value.Date = dtpDepartureTime.Value.Date Then
+            If dtpLandingTme.Value.Hour < dtpDepartureTime.Value.Hour Then
+                blnUpdated = True
+                dtpLandingTme.Value = dtpLandingTme.Value.AddHours(24)
+                'Exit Sub
+            End If
+        Else
+            If dtpLandingTme.Value.Hour > dtpDepartureTime.Value.Hour Then
+                blnUpdated = True
+                dtpLandingTme.Value = dtpLandingTme.Value.AddDays(-1)
+                'Exit Sub
+            End If
+        End If
+        blnUpdated = False
     End Sub
 End Class
